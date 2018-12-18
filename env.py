85537186833ff6f1acd8f52d917c5fb595c4ef35
liziniu <374387855@qq.com>
2018-12-18 16:28:57 +0800

    fix reset

--- env.py
+++ env.py
@@ -6,10 +6,21 @@ class Env:
         self.dic_env_config = dic_env_config
 
         self.env = gym.make(self.dic_env_config["ENV_NAME"])
-        self.env.seed(self.dic_env_config["RANDOM_SEED"])
+        self.env.seed(self.dic_env_config["GYM_SEED"])
 
     def reset(self):
         return self.env.reset()
 
     def step(self, a):
         return self.env.step(a)
+
+    def render(self):
+        self.env.render()
+
+    @property
+    def state_dim(self):
+        return self.env.observation_space.shape[0]
+
+    @property
+    def action_dim(self):
+        return self.env.action_space.n
